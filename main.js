dddc661409fd909ccbc93e741b35501d3351b3eb
jonathanobi47 <jonathanobi47@gmail.com>
2025-06-27 09:47:03 +0000

    Added Content

---
+++ main.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('popup').style.display='block'
+  }
+  
+  function closePopup() {
+    alert('Thank you for subscribing!')
+    document.getElementById('popup').style.display='none'
+  }
